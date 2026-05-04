878568cd1f0adb06d71aedf7fa08aacfc49dce88
tg-agent <268907238+tg-agent@users.noreply.github.com>
2026-05-04 08:19:37 -0700

    Revert "fix: consolidate adapter/serializer imports to ember-data entry point"
    This reverts commit dede5f0531020ab5f9219986c2c4aa39c0522e11.

--- app/adapters/rfc.ts
+++ app/adapters/rfc.ts
@@ -1,4 +1,4 @@
-import JSONAPIAdapter from 'ember-data/adapter/json-api';
+import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import { getOwner } from '@ember/application';
 import type RfcGateway from '../gateways/rfc-gateway';
 
